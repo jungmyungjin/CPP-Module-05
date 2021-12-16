703122d4c3844e284d1ed54c50133b4939bd680b
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-12-16 20:22:10 +0900

    ex00 빼먹은 기능 부분 추가

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -1,24 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjung <mjung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 18:48:40 by mjung             #+#    #+#             */
+/*   Updated: 2021/12/16 20:20:46 by mjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 #include <string>
 #include <iostream>
+#include <exception>
 
-class Bureaucrat
-{
+class Bureaucrat {
 public:
 	Bureaucrat();
-	Bureaucrat(std::string _name, int _grade);
-	~Bureaucrat();
+	Bureaucrat(const std::string _name, int _grade);
+
+	virtual ~Bureaucrat();
+
 	Bureaucrat(const Bureaucrat &target);
+
 	Bureaucrat &operator=(const Bureaucrat &target);
 
 	std::string getName() const;
 	unsigned getGrade() const;
-	void setIncreaseGrade();	// 등급을 높인다
-	void setDecreaseGrade();	// 등급을 낮춘다
+	void setIncreaseGrade();    // 등급을 높인다
+	void setDecreaseGrade();    // 등급을 낮춘다
+
+	class GradeTooHighException : public std::exception {
+	public:
+		const char *what(void) const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	public:
+		const char *what(void) const throw();
+	};
 
 private:
-	std::string name_;
+	const std::string name_ ;
 	int grade_;
 
 };
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,23 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjung <mjung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 20:19:10 by mjung             #+#    #+#             */
+/*   Updated: 2021/12/16 20:19:10 by mjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include "Bureaucrat.hpp"
 
+/*
+ * Bureaucrat : 관료 클래스
+ * 등급 : 1 ~ 150
+ * 최저 등급 = 150
+ * 최고 등급 = 1
+ */
 int main(void)
 {
 	try
 	{
-//		Bureaucrat human("name", 1150);
-//		Bureaucrat human("name", -1);
-//		Bureaucrat human("name", 150);
+//		0. 기본 동작
+		Bureaucrat human("Judy", 50);
+		std::cout << human << std::endl;
+
+//		1. 범위 초과 등급으로 선언 (에러)
+//		Bureaucrat human("Judy", 1150);
+
+//		2. 범위 미만 등급으로 선언 (에러)
+//		Bureaucrat human("Judy", -1);
+
+//		3. 등급 올리는 함수 사용
+//		Bureaucrat human("Judy", 5);
+//		human.setIncreaseGrade();
+//		std::cout << human << std::endl;
+
+//		4. 등급 낮추는 함수 사용
+//		Bureaucrat human("Judy", 5);
 //		human.setDecreaseGrade();
-//		Bureaucrat human("name", 0);
+//		std::cout << human << std::endl;
+
+//		5. 범위 외 등급으로 높힐때 (에러)
+//		Bureaucrat human("Judy", 1);
 //		human.setIncreaseGrade();
 
-		Bureaucrat human("name", 3);
-		std::cout << human << std::endl;
+//		6. 범위 외 등급으로 낮출떄 (에러)
+//		Bureaucrat human("Judy", 150);
+//		human.setDecreaseGrade();
+
 	}
-	catch (const char *str)
+	catch (std::exception & e)
 	{
-		std::cout << str << std::endl;
+		std::cerr << e.what() << std::endl;
 	}
 
 	return (0);
